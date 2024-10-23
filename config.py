5543c1101a5aa6d479c2e3c229c74e7c14b2568f
USPEY <umeshpatharkar@vctcpune.co.in>
2024-10-23 21:15:03 +0530

    add

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
 import os
-MODEL_FILE_PATH = os.path.join("Medical_Insurance","dt_model.pkl")
-STD_FILE_PATH = os.path.join("Medical_Insurance","std_scalar.pkl")
-JSON_FILE_PATH = os.path.join("Medical_Insurance","project_data.json")
+MODEL_FILE_PATH = os.path.join("
